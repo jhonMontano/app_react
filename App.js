81b3bb057c9717a7ef26a72f437ba5125e928195
Alexander <jhon.montano0327@gmail.com>
2023-08-17 20:26:56 -0500

    Empieza proyecto con react y expo

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { RootTagContext, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Welcome Jhon Alexander</Text>
+      <Text>Open up App.js to start working on your app! Welcome Jhon Alexander</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
